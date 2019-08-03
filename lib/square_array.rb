43f5e820e872dc7158f4e36751f5e27887aa7325
James Sharpe <jameswrightsharpe@gmail.com>
2019-08-03 17:50:16 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
   squared = []  
   i=0
-  while array[i] do
-    squared.push(array[i].shift ** 2)
+  while array.size > 0 do
+    squared.push(array.shift ** 2)
     i += 1
   end
   return squared
